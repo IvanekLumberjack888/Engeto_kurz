63d91d7e052e92f5d0b9b93f439a6a2093bc5b73
Ivaneklumberjack888 <ivousd@gmail.com>
2025-04-21 14:54:30 +0200

    add

---
+++ 21. 4. 2025 - 4.py
@@ -0,0 +1,6 @@
+#Napiš dvoumístné čílo
+cislo = input("Napiště dvoumístné číslo:\n")
+prvni = int(cislo[0])
+druhe = int(cislo[1])
+soucet = int(prvni + druhe) #nebo nemusim mit soucet a napisu to rovnou do printu(prvni+druhe)
+print(soucet)
