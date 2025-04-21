7e4ae030c80dab4f864a26c2c1986991287d0e0e
Ivaneklumberjack888 <ivousd@gmail.com>
2025-04-21 14:17:45 +0200

    přidáno

--- 21. 4. 2025 - 2.py
+++ 21. 4. 2025 - 2.py
@@ -1,4 +1,4 @@
 print("Vítej v aplikaci na generování vtipných jmen")
 krestni_jmeno = input("Jaké je tvé křestní jméno?\n")
 vtipne_prijmeni = input("Jaká je tvá typická vlstnost? Napiš ji s velkým písmenem na začátku\n")
-print("Tvoje vtipné jméno je "+ krestni_jmeno + " " + vtipne_prijmeni)
+print("Tvoje vtipné jméno je "+ krestni_jmeno + " " + vtipne_prijmeni) 
---
+++ 21. 4. 2025 - 3.py
@@ -0,0 +1,18 @@
+# String
+print("ahoj"[3])
+print("20" + "25")
+print("Ah" + "o" + "j")
+# Integer
+print(10 + 15)
+print(123_456)
+print(123456)
+# Float
+print(3.14)
+print(0.9987655)
+# Bolean
+print(1 >= 1)
+print(type(True))
+
+age = 40
+print(type(age))
+print("Ahoj já jsem David a je mi " + str(age) + " let.")
