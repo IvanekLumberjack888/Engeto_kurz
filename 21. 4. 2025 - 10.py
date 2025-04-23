757d1d7c791cceaffa1f85418992d3ec2d4c8a84
Ivaneklumberjack888 <ivousd@gmail.com>
2025-04-23 21:00:16 +0200

    add

--- 21. 4. 2025 - 10.py
+++ 21. 4. 2025 - 10.py
@@ -1,3 +1,4 @@
 print("Dnes uz netaham. ")
 smajlik = "😒"
 print(smajlik * 10)
+23. 4. 2025 20:46:10 
---
+++ 23. 4. 2025 - 1.py
@@ -0,0 +1,17 @@
+import calendar
+print(calendar)
+
+from calendar import *
+print(calendar)
+
+from calendar import day_name
+print(calendar)
+
+from calendar import day_name as nden
+print(nden)
+
+import calendar as kal
+print(kal)
+
+import os as o
+print(o)
